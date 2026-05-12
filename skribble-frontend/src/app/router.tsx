bcc49174561ca6a0558f88154676a7f432ae20f2
Nahom <nahiyo86@gmail.com>
2026-05-12 23:57:46 +0300

    chore: improve documentation

--- skribble-frontend/src/app/router.tsx
+++ skribble-frontend/src/app/router.tsx
@@ -1,9 +1,11 @@
 // src/app/router.tsx
+
 import { createBrowserRouter } from "react-router-dom"
 import JoinPage from "../features/lobby/JoinPage"
 import GamePage from "../features/game/GamePage"
 
 export const router = createBrowserRouter([
   { path: "/", element: <JoinPage /> },
+  { path: "/join/:roomID", element: <JoinPage /> },
   { path: "/game", element: <GamePage /> },
 ])
