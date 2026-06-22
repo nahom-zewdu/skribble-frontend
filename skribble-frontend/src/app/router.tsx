bfcfb7886572959e029ef70ed0c8d1bdcd403508
Nahom <nahiyo86@gmail.com>
2026-06-22 17:29:24 +0300

    chore: add route for dashboard page

--- skribble-frontend/src/app/router.tsx
+++ skribble-frontend/src/app/router.tsx
@@ -3,9 +3,11 @@
 import { createBrowserRouter } from "react-router-dom"
 import JoinPage from "../features/lobby/JoinPage"
 import GamePage from "../features/game/GamePage"
+import DashboardPage from "../features/dashboard/DashboardPage"
 
 export const router = createBrowserRouter([
   { path: "/", element: <JoinPage /> },
   { path: "/join/:roomID", element: <JoinPage /> },
   { path: "/game", element: <GamePage /> },
+  { path: "/dashboard", element: <DashboardPage /> },
 ])
